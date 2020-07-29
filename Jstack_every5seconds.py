efddcbc7c7f0f2c55986cc6d1a4f1948b50e10d8
abiadenwala <67893377+abiadenwala@users.noreply.github.com>
2020-07-29 15:58:39 -0500

    Every 5 seconds for 5 minutes

--- Jstack_every5seconds.py
+++ Jstack_every5seconds.py
@@ -1,8 +1,14 @@
 import time
 import os
+#number of iterations we need to go through the loop (In this case its 60 since i want this script to collect output for 5 minutes every 5 seconds).
+iteration = 0
 while True:
-    Outputfile = 'jstack_' + time.strftime("%m%d-%H%M%S")
+    if(iteration == 60):
+        break
+    else:
+        
+        Outputfile = 'jstack_' + time.strftime("%m%d-%H%M%S")
 # Jstack on Java pid
-    jstackoutput = os.system("jstack <JAVA_PID> >> Output_" + Outputfile)
+        jstackoutput = os.system("jstack <JAVA_PID> >> Output_" + Outputfile)
 # Runs the command every 5 seconds
-    time.sleep(5)
+        time.sleep(5)
